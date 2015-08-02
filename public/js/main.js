0db1e4e838a11ff0f374b4f9651942bb20e084b9
Ann Yuan <ayuan@thisisgrow.com>
2015-08-02 12:25:45 -0400

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 var util = require('./util');
+var api = require('./api');
 
-util();
+document.querySelector("#vote-button").addEventListener("click", () => {
+  api.post('http://oughtness-49671.onmodulus.net/vote', {question_id: 0, data: 20}, (data) => {
+    console.log("SUCCESS");
+    console.log(data);
+  });
+});
