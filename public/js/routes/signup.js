1e603fb5643a8c2d95d1ab5c87f44265885c2186
Ann Yuan <ayuan@thisisgrow.com>
2015-08-11 04:52:30 -0400

    form handler

--- public/js/routes/signup.js
+++ public/js/routes/signup.js
@@ -1,8 +1,23 @@
+var api = require('../api');
+var mediator = require('../mediator');
+
 module.exports = {
   initialize() {
 
   },
   start() {
-
+    mediator.subscribe("window_click", (e) => {
+      if(e.target.getAttribute("id") === "signup-button") {
+        e.preventDefault();
+        api.post('/signup', {
+          username: 'ann',
+          email: 'ann@ann.com',
+          password: 'yuan'
+        }, (data) => {
+          console.log("SIGNED UP");
+          console.log(data);
+        });
+      }
+    });
   }
 };
