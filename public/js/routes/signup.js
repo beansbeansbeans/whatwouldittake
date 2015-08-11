cc61db2f80305a5fd6c96ec27713ec454ede62b0
Ann Yuan <ayuan@thisisgrow.com>
2015-08-11 07:28:29 -0400

    dynamically send form data

--- public/js/routes/signup.js
+++ public/js/routes/signup.js
@@ -9,15 +9,18 @@ module.exports = {
     mediator.subscribe("window_click", (e) => {
       if(e.target.getAttribute("id") === "signup-button") {
         e.preventDefault();
+        var form = d.gbID("signup-form");
         api.post('/signup', {
-          username: 'ann',
-          email: 'ann@ann.com',
-          password: 'yuan'
+          username: form.querySelector('[name="username"]').value,
+          email: form.querySelector('[name="email"]').value,
+          password: form.querySelector('[name="password"]').value
         }, (data) => {
           if(data.success) {
             console.log("SIGNED UP");
+            page.show('/', null, false);
           } else {
             console.log("FAILED TO SIGN UP");
+            // render error message
           }
           console.log(data);
         });
