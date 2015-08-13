14e87a0625c985267c9b13c0e3d9883fbebb7777
Ann Yuan <ayuan@thisisgrow.com>
2015-08-13 07:48:00 -0400

    starting to create virtual dom

--- public/js/routes/login.js
+++ public/js/routes/login.js
@@ -1,11 +1,38 @@
+var h = require('virtual-dom/h');
+var diff = require('virtual-dom/diff');
+var patch = require('virtual-dom/patch');
+var createElement = require('virtual-dom/create-element');
 var api = require('../api');
 var mediator = require('../mediator');
+
+var rootNode;
+var tree;
+
+var updateState = () => {
+  var newTree = render();
+  var patches = diff(tree, newTree);
+  rootNode = patch(rootNode, patches);
+  tree = newTree;
+};
+
+var render = () => {
+  return h('div#login-page', [
+    h('div.title', 'login to the app'),
+    h('form', [
+      h('input')
+    ])
+  ]);
+};
 
 module.exports = {
   initialize() {
 
   },
   start() {
+    tree = render();
+    rootNode = createElement(tree);
+    d.gbID('content').appendChild(rootNode);
+
     mediator.subscribe("window_click", (e) => {
       if(e.target.getAttribute("id") === "login-button") {
         e.preventDefault();
