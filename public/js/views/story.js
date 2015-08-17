3c0c53d0b20dc912bea914f6413206eb1f4fa43d
Ann Yuan <ayuan@thisisgrow.com>
2015-08-17 07:38:59 -0400

    display entry stuff on story page

--- public/js/views/story.js
+++ public/js/views/story.js
@@ -3,18 +3,30 @@ var api = require('../api');
 var mediator = require('../mediator');
 var view = require('../view');
 
+var storyState = {};
+
 class storyView extends view {
   start(ctx) {
     super.start();
 
     api.get('/story/' + ctx.params.id, (error, data) => {
-      console.log("GOT STORY");
-      console.log(data);
+      storyState.story = data.data;
+      this.updateState();
     });
   }
 
   render() {
-    return h('div#story-view', 'HEY HERE IS THE STORY');
+    if(!storyState.story) { return h('div'); }
+    
+    return h('div#story-view', [
+      h('div.user', storyState.story.user.username),
+      storyState.story.entries.map((entry) => {
+        return h('div.entry', [
+          h('div.date', entry.date),
+          h('div.feeling', entry.feeling)
+        ])
+      })
+    ]);
   }
 }
 
