f127b02c93f5888bb13cf3caf095be77eede032d
Ann Yuan <ayuan@thisisgrow.com>
2015-08-17 06:22:04 -0400

    improved error handling

--- public/js/views/signup.js
+++ public/js/views/signup.js
@@ -15,41 +15,71 @@ var state = {
 
 var errorMessages = {
   username: {
-    tooShort: 'Usernames must be at least three characters long.',
-    containsSpaces: 'Usernames may not contain spaces.',
-    alreadyExists: 'This username has already been taken.'
+    invalid(val) {
+      if(val.length > 2 && val.indexOf(' ') === -1) {
+        return true;
+      }
+      return 'Usernames must be at least 3 characters long and may not contain spaces.';
+    }
   },
   email: {
-    alreadyExists: 'A user with this email address already exists.',
-    invalid: 'Sorry, that does not appear to be a valid email address.'
+    invalid(val) {
+      var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+      if(re.test(val)) { return true; }
+      return 'Sorry, that does not appear to be a valid email address.'
+    }
   },
   password: {
-    tooShort: 'Passwords must be at least 5 characters long.',
-    containsSpaces: 'Passwords may not contain spaces.'
+    invalid(val) {
+      if(val.length > 4 && val.indexOf(' ') === -1) {
+        return true;
+      }
+      return 'Passwords must be at least 5 characters long and may not contain spaces.';
+    }
   }
 };
 
 class signupView extends view {
+
+  validate() {
+    state.attemptingSubmission = true;
+    state.fieldStatus = {
+      username: false,
+      email: false,
+      password: false
+    };
+
+    Object.keys(errorMessages).forEach((field) => {
+      var displayMsg;
+
+      Object.keys(errorMessages[field]).every((error) => {
+        var result = errorMessages[field][error](d.querySelector('[name="' + field + '"]').value);
+        if(result !== true) {
+          state.fieldStatus[field] = result;
+        }
+
+        return result === true;
+      });
+    });
+
+    this.updateState();
+
+    return Object.keys(state.fieldStatus).every(x => !state.fieldStatus[x]);
+  }
+
   start() {
     super.start();
 
     mediator.subscribe("window_click", (e) => {
       if(e.target.getAttribute("id") === "signup-button") {
         e.preventDefault();
+        
         var form = d.gbID("signup-form"),
           username = form.querySelector('[name="username"]').value,
           email = form.querySelector('[name="email"]').value,
           password = form.querySelector('[name="password"]').value;
 
-        state.attemptingSubmission = true;
-        state.fieldStatus = {
-          username: !username,
-          email: !email,
-          password: !password
-        };
-        this.updateState();
-
-        if(Object.keys(state.fieldStatus).every(x => !state.fieldStatus[x])) {
+        if(this.validate()) {
           api.post('/signup', {
             username: username,
             email: email,
@@ -72,34 +102,34 @@ class signupView extends view {
       h('div.title', 'signup for the app'),
       h('form#signup-form', { method: "post" }, [
         h('div.input-wrapper', {
-          dataset: { error: state.fieldStatus.username }
+          dataset: { error: state.fieldStatus.username !== false }
         }, [
           h('input', {
             type: "text",
             name: "username",
             placeholder: "username"
           }),
-          h('div.error', 'This field is required.')
+          h('div.error', state.fieldStatus.username)
         ]),
         h('div.input-wrapper', {
-          dataset: { error: state.fieldStatus.email }
+          dataset: { error: state.fieldStatus.email !== false }
         }, [
           h('input', {
             type: "text",
             name: "email",
             placeholder: "email"
           }),
-          h('div.error', 'This field is required.')
+          h('div.error', state.fieldStatus.email)
         ]),
         h('div.input-wrapper', {
-          dataset: { error: state.fieldStatus.password }
+          dataset: { error: state.fieldStatus.password !== false }
         }, [
           h('input', {
             type: "password",
             name: "password",
             placeholder: "password"
           }),
-          h('div.error', 'This field is required.')
+          h('div.error', state.fieldStatus.password)
         ]),
         h('input#signup-button', {
           type: "submit",
