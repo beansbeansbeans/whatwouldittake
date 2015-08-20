7827ba9c15f5b688dc93a0654b502ac1f34002b4
Ann Yuan <ayuan@thisisgrow.com>
2015-08-20 06:01:43 -0400

    add date of last entry

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -30,6 +30,11 @@ class indexView extends view {
         return h('li', [
           username,
           h('div.entries-count', util.pluralize(story.entries.length, 'entry', 'entries')),
+          h('div.date', moment.utc(story.entries.sort((a, b) => {
+            if(a.date > b.date) { return -1; }
+            if(a.date < b.date) { return 1; }
+            return 0;
+          })[0].date, 'X').format('YYYY MM DD')),
           h('a', {
             href: 'story/' + story._id
           }, 'goto')
