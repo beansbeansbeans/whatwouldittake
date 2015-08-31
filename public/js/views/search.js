32b5339a1a6f1d0010f8939277d58650b5176960
Ann Yuan <ayuan@thisisgrow.com>
2015-08-31 07:23:34 -0400

    canvas draw

--- public/js/views/search.js
+++ public/js/views/search.js
@@ -7,11 +7,85 @@ var view = require('../view');
 var util = require('../util');
 var sparklineSubview = require('./subviews/sparkline');
 
+var pos = {
+  x: 0,
+  y: 0
+};
+
+var offset = {
+  x: 0,
+  y: 0
+}
+
+var ctx;
+var dragging = false;
+
+var draw = (e) => {
+  if(!dragging) { return; }
+
+  ctx.beginPath();
+
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = '#c0392b';
+
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+
+  ctx.stroke();
+}
+
+var setPosition = (e) => {
+  pos.x = e.clientX - offset.x;
+  pos.y = e.clientY - offset.y;
+}
+
+var handleMouseDown = (e) => {
+  dragging = true;
+  setPosition(e);
+}
+
+var handleMouseUp = (e) => {
+  dragging = false;
+}
+
 class searchView extends view {
+
+  start() {
+    super.start();
+
+    window.addEventListener('mousemove', draw);
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mouseup', handleMouseUp);
+  }
+
+  stop() {
+    super.stop();
+
+    window.removeEventListener('mousemove', draw);
+    window.removeEventListener('mousedown', handleMouseDown);
+    window.removeEventListener('mouseup', handleMouseUp);
+  }
+
+  mount() {
+    super.mount();
+
+    var canvas = d.qs('canvas');
+    ctx = canvas.getContext('2d');
+
+    var rect = canvas.getBoundingClientRect();
+    offset.x = rect.left;
+    offset.y = rect.top;
+  }
 
   render() {
     return h('div#search', [
-      h('div', 'this is the search page')
+      h('div.title', 'this is the search page'),
+      h('canvas', {
+        width: 600,
+        height: 300
+      })
     ]);
   }
 }
