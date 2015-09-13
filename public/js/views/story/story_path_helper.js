92e9cb402a2371056acaeef565d16036e397d669
Ann Yuan <ayuan@thisisgrow.com>
2015-09-13 08:19:26 -0400

    cleanup

--- public/js/views/story/story_path_helper.js
+++ public/js/views/story/story_path_helper.js
@@ -4,15 +4,11 @@ module.exports = {
     
     var horizontalBuffer = typeof dimensions.horizontalBuffer === 'undefined' ? 10 : dimensions.horizontalBuffer,
       verticalBuffer = typeof dimensions.verticalBuffer === 'undefined' ? 10 : dimensions.verticalBuffer,
-      minFeeling = Math.min.apply(Math, story.entries.map(x => x.feeling)),
-      maxFeeling = Math.max.apply(Math, story.entries.map(x => x.feeling)),
       feelings = story.entries.map(x => x.feeling).slice(0).reverse();
 
-    var yScale = d3.scale.linear().domain([minFeeling, maxFeeling])
+    var yScale = d3.scale.linear().domain([0, 100])
       .range([verticalBuffer, dimensions.height - verticalBuffer]),
-      y = (d) => {
-        return dimensions.height - yScale(d)
-      },
+      y = d => dimensions.height - yScale(d),
       x = (d, i) => {
         return horizontalBuffer + i * (dimensions.width - horizontalBuffer * 2) / (story.entries.length - 1);
       },
