1b727e84c92695d90b278b7be77d7cc32e4a2414
Ann Yuan <ayuan@thisisgrow.com>
2015-09-30 19:13:58 -0400

    stuff

--- public/js/views/about.js
+++ public/js/views/about.js
@@ -15,6 +15,7 @@ class aboutView extends view {
     return h('div#about-view', [
       h('div.main', [
         h('h1', 'About'),
+        h('div.subtitle', 'This is a concept for a social network.'),
         h('div.description', "Social media has become a tool for projecting the best version of ourselves to the world. Someone feeling unhappy may get the impression from their Facebook newsfeed that they're alone in their experience."),
         h('div.description', "I built Stories of as a way for people to share their moods, good or bad.")
       ])
