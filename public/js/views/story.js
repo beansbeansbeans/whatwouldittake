b925368dc5c4919d08c41300ec260f0f4e674e1f
Ann Yuan <ayuan@thisisgrow.com>
2015-09-30 18:45:23 -0400

    assurances

--- public/js/views/story.js
+++ public/js/views/story.js
@@ -96,7 +96,9 @@ class storyView extends view {
 
       this.updateState();
       this.handleResize();
-      d.qs('.svg-scroller').scrollLeft = d.qs('svg').offsetWidth - d.qs('.svg-scroller').offsetWidth + d.qs('.svg-scroller .label').offsetWidth;
+      if(d.qs('svg')) {
+        d.qs('.svg-scroller').scrollLeft = d.qs('svg').offsetWidth - d.qs('.svg-scroller').offsetWidth + d.qs('.svg-scroller .label').offsetWidth;
+      }
     });
 
     var thisIndex = _.findIndex(state.get('stories'), d => d._id === +ctx.params.id);
