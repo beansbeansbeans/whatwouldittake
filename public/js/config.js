d76d7442c37e7b264d6ed74dc439abe25708c997
Ann Yuan <ayuan@thisisgrow.com>
2015-10-06 07:39:32 -0400

    arrows

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,8 @@ module.exports = {
     maxDate: moment().toDate(),
     setDefaultDate: true,
     i18n: {
-      previousMonth: '<',
-      nextMonth: '>',
+      previousMonth: '',
+      nextMonth: '',
       months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       weekdaysShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
