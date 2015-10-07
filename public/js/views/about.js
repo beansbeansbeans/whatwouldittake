85b93bfffe1a0e174937c2bfc96ba3613ecf8f8b
Ann Yuan <ayuan@thisisgrow.com>
2015-10-07 07:23:25 -0400

    bones

--- public/js/views/about.js
+++ public/js/views/about.js
@@ -21,7 +21,7 @@ class aboutView extends view {
           h("span", "I built Stories of as a way for people to share their moods, good or bad. "),
           h("span.bold", "I hope this will achieve two effects:")
         ]),
-        h('div.description', "(1) I want people who are feeling unhappy to see this as a safe place for sharing."),
+        h('div.description', "(1) I want people who are feeling unhappy to see this as a safe place for sharing. That's why users can choose to hide their names on their posts, and why notes are optional. Sometimes it's nice just to be able to acknowledge that you're not feeling your best."),
         h('div.description', "(2) I want people who are feeling unhappy to see that they're not alone. When you're unhappy, it can easily seem like you'll always feel this way. I want the site to serve as proof that people get over their unhappiness. Stories are defined by a sparkline that tracks changes in the user's mood, and users can search the site for stories matching a particular mood trajectory.")
       ])
     ]);
