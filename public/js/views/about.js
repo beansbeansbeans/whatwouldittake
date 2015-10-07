9e60ad724b3c61cabc3ab34120a28737c79d23af
Ann Yuan <ayuan@thisisgrow.com>
2015-10-07 08:06:38 -0400

    copy

--- public/js/views/about.js
+++ public/js/views/about.js
@@ -17,17 +17,14 @@ class aboutView extends view {
         h('h1', 'About'),
         h('div.subtitle', 'This is a concept for a social network.'),
         h('div.description', "According to our Facebook newsfeeds, everyone around us is leading happy and fulfilling lives. We use social media to project the best versions of ourselves to the world. This means that when we're unhappy, it can feel like we're alone in our experience."),
-        h('div.description', [
-          h("span", "We built Stories of to address this problem. "),
-          h("span", "The site has two purposes:")
-        ]),
+        h('div.description', "STORIES OF aims to address this problem. The site is: "),
         h('div.description', [
           h('span.bold', '(1) A tool for people to share their feelings, good or bad. '),
-          h('span', "I want people who are feeling unhappy to see this as a safe place for sharing. That's why users can choose to hide their names on their posts, and why notes are optional. Sometimes it's nice just to be able to acknowledge that you're not feeling your best.")
+          h('span', "We wanted people feeling unhappy to see this as a safe place. That's why users can choose to hide their names on their posts, and why notes are optional. Sometimes it's nice just to be able to acknowledge that you're not feeling your best.")
         ]),
         h('div.description', [
-          h('span.bold', "(2) A searchable record of people's wide-ranging feelings. "),
-          h('span', "I want people who are feeling unhappy to see that they're not alone. When you're unhappy, it can sometimes feel like you'll never bounce back. I want the site to serve as proof that people get over their unhappiness. Stories are defined by a sparkline that tracks changes in the user's mood, and users can search the site for stories matching a particular mood trajectory.")
+          h('span.bold', "(2) A searchable database of people's wide-ranging feelings. "),
+          h('span', "We wanted people to see that they're not alone, no matter how they're feeling. When you're unhappy, it can sometimes feel like you'll never bounce back. We wanted the site to serve as proof that people get over their unhappiness. Stories are defined by a sparkline that tracks changes in the user's mood, and users can search the site for stories matching a particular mood trajectory.")
         ])
       ])
     ]);
