23bcbc9ab7caa232a6289837fbd25f3e875edb0e
Ann Yuan <ayuan@thisisgrow.com>
2015-10-10 08:44:52 -0400

    improvements

--- public/js/views/about.js
+++ public/js/views/about.js
@@ -20,11 +20,15 @@ class aboutView extends view {
         h('div.description', "STORIES OF aims to address this problem. The site is: "),
         h('div.description', [
           h('span.bold', '(1) A tool for people to share their feelings, good or bad. '),
-          h('span', "The site is composed of posts in which users grade their feelings at a particular moment on a scale of one to 100. A user's posts over time form a story of their feelings. Text is optional, and users can post anonymously. This way, a post is simply an acknowledgment of a user's feelings, and stories' emphasis is on how users' feelings change over time.")
+          h('span', "The site is composed of posts in which users grade their feelings at a particular moment on a scale of one to 100. A user's posts over time form a story of their feelings. Text is optional, and users can post anonymously. This way, a post is simply an acknowledgment of a user's feelings.")
         ]),
         h('div.description', [
           h('span.bold', "(2) A searchable database of people's wide-ranging feelings. "),
-          h('span', "When we're unhappy it can feel like we'll never bounce back. What if in those moments we could browse stories of people recovering from their unhappiness? On STORIES OF, users can search for stories matching particular trajectories of feeling. For example, users can search for stories of people feeling sad but then getting happier, or vice versa.")
+          h('span', [
+            h('span', "When we're unhappy it can feel like we'll never bounce back. What if in those moments we could browse stories of people recovering from their unhappiness? On STORIES OF, users can "),
+            h('a', { href: '/search' }, 'search'),
+            h('span', ' for stories matching particular trajectories of feeling. For example, users can search for stories of people feeling sad but then getting happier, or vice versa.')
+          ])
         ])
       ])
     ]);
