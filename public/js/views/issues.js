2b3006434c33c505e14a8a50071cd4fabb6eed2c
Ann Yuan <ayuan@thisisgrow.com>
2015-10-21 20:29:22 -0400

    changes

--- public/js/views/issues.js
+++ public/js/views/issues.js
@@ -11,17 +11,36 @@ class issuesView extends view {
   start() {
     super.start();
 
+    _.bindAll(this, 'handleClick');
+
     api.get('/issues', (err, data) => {
       viewState.issues = data.data;
       this.updateState();
     });
+
+    mediator.subscribe("window_click", this.handleClick);
+  }
+
+  handleClick(e) {
+    if(e.target.classList.contains('issue')) {
+      page.show('vote/' + e.target.dataset.slug);
+    }
+  }
+
+  stop() {
+    super.stop();
+    mediator.unsubscribe("window_click", this.handleClick);
   }
 
   render() {
     return h('div#issues-view', [
       h('div', 'The issues'),
       viewState.issues.map((d) => {
-        return h('div', d.slug);
+        return h('div.issue', {
+          dataset: {
+            slug: d.slug
+          }
+        }, d.slug);
       })
     ]);
   }
