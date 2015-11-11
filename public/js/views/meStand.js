6ea7cf3c3c5477223af51506cb4810799187de58
Ann Yuan <ayuan@thisisgrow.com>
2015-11-11 07:23:14 -0500

    yaxislabels

--- public/js/views/meStand.js
+++ public/js/views/meStand.js
@@ -22,6 +22,7 @@ class meStandView extends view {
   didRender() {
     if(!viewState.issue) { return; }
 
+    var yAxisWidth = 25;
     var dateBuffer = 25;
     var chartWrapper = d3.select(".chart-contents");
     var availableWidth = chartWrapper[0][0].offsetWidth;
@@ -33,22 +34,46 @@ class meStandView extends view {
     var yScale = d3.scale.linear().domain([minY, maxY]).range([0, availableHeight - dateBuffer]);
     var y = d => yScale(d);
     var x = (d, i) => {
-      return i * (availableWidth / (viewState.issue.data.aff.length - 1));
+      return yAxisWidth + i * (availableWidth / (viewState.issue.data.aff.length - 1));
     };
     var line = d3.svg.line().x(x).y(y).interpolate("cardinal");
 
     var chartEl = chartWrapper.select("svg");
-    chartEl.attr("width", availableWidth).attr("height", availableHeight);
+    chartEl.attr("width", availableWidth + yAxisWidth).attr("height", availableHeight)
+      .style("margin-left", -1 * yAxisWidth);
 
     var sparklines = chartEl.selectAll("path").data([viewState.issue.data.aff, viewState.issue.data.neg]);
     sparklines.enter().append("path");
     sparklines.attr("d", line);
 
-    chartEl.append("g").attr("class", "date-markers");
+    if(!d.qs(".date-markers")) {
+      chartEl.append("g").attr("class", "date-markers");
+    }
 
     var dateMarkers = chartEl.select(".date-markers").selectAll("text").data(viewState.issue.data.times);
     dateMarkers.enter().append("text");
     dateMarkers.text(d => moment.unix(d).format('M/YY')).attr("x", x).attr("y", availableHeight - 5);
+
+    if(!d.qs(".y-axis-markers")) {
+      chartEl.append("g").attr("class", "y-axis-markers");
+    }
+
+    var yAxisData = [minY];
+    var yAxisCount = 5;
+    var yAxisLabelHeight = 10;
+    for(var i=1; i<yAxisCount ; i++) {
+      if(i === yAxisCount - 1) {
+        yAxisData.push(maxY);
+      } else {
+        yAxisData.push(minY + i * ((maxY - minY) / (yAxisCount - 1)));
+      }
+    }
+
+    var yAxisMarkers = chartEl.select(".y-axis-markers").selectAll("text").data(yAxisData);
+    yAxisMarkers.enter().append("text");
+    yAxisMarkers.text(d => Math.round(d)).attr("y", (d, i) => { 
+      return (availableHeight - dateBuffer) - i * ((availableHeight - dateBuffer - yAxisLabelHeight) / (yAxisData.length - 1));
+    });
   }
 
   handleResize() {
