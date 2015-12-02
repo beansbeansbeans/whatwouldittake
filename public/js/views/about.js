2bdec87d8710c408bd234d38f914b893db4120e2
Ann Yuan <ayuan@thisisgrow.com>
2015-12-01 19:42:50 -0500

    about page copy

--- public/js/views/about.js
+++ public/js/views/about.js
@@ -15,8 +15,10 @@ class aboutView extends view {
     return h('div#about-view', [
       h('div.main', [
         h('h1', 'About'),
-        h('div.subtitle', 'What is this place?'),
-        h('div.description', "We created this site because we wanted to help people become better informed about their opinions.")
+        h('div.subtitle', 'Our goal is to help people become better informed about their opinions.'),
+        h('div.description', "What Would It Take (WWIT) encourages users to consider beliefs that contradict their own by asking them to describe what evidence could change their beliefs. WWIT focuses on hot button issues like gun control and climate change, where people have strong opinions and rarely change their minds."),
+        h('div.description', "Sometimes a user will be able to articulate mind-changing evidence - for example, a user may believe stricter gun laws would not reduce mass shootings, but he may also acknowledge that he would change his mind if shown evidence that criminals would obey gun laws. Thus, WWIT gives the user a way to consider opposing beliefs."),
+        h('div.description', "Other times, a user will not be able to articulate any evidence that would change his mind. WWIT may then prompt the uncomfortable realization that his belief cannot be refuted because it is based on emotion, not evidence.")
       ])
     ]);
   }
